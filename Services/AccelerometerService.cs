41ee9eddc8bdcffa811313464bc7f69dd1f9ad54
ZofiaLor <zosia984@gmail.com>
2025-12-04 18:09:29 +0100

    debug line

--- Services/AccelerometerService.cs
+++ Services/AccelerometerService.cs
@@ -10,9 +10,11 @@ namespace MazeGame.Services
         private readonly Accelerometer _accelSensor;
         private Sprite? _movingObject;
         private readonly float _sensitivity;
+        public Vector2 accReading; // dbg
 
         public AccelerometerService(float sensitivity = 0.1f)
         {
+            accReading = Vector2.Zero; // dbg
             _sensitivity = sensitivity;
             _accelSensor = new Accelerometer();
             _accelSensor.CurrentValueChanged += ChangeVelocity;
@@ -27,6 +29,8 @@ namespace MazeGame.Services
             if (_movingObject is null)
                 return;
             //Console.WriteLine($"X: {e.SensorReading.Acceleration.X}, Y: {e.SensorReading.Acceleration.Y}, Z: {e.SensorReading.Acceleration.Z}");
+            accReading.X = -e.SensorReading.Acceleration.X; // dbg
+            accReading.Y = e.SensorReading.Acceleration.Y; // dbg
             _movingObject.Velocity += _sensitivity * new Vector2(-e.SensorReading.Acceleration.X, e.SensorReading.Acceleration.Y);
         }
     }
